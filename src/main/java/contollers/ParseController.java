5b8e3d8f735d7355aa67c58906b6cc49df493f12
braxtonhall <braxtonjhall@gmail.com>
2019-08-18 22:01:18 -0700

    Changed method longName

--- src/main/java/contollers/ParseController.java
+++ src/main/java/contollers/ParseController.java
@@ -5,17 +5,9 @@ import com.felixgrund.codeshovel.services.impl.CachingRepositoryService;
 import com.felixgrund.codeshovel.util.Utl;
 import com.felixgrund.codeshovel.wrappers.Commit;
 import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Modifier;
-import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.Parameter;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.expr.ObjectCreationExpr;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ExpressionStmt;
-import com.github.javaparser.ast.stmt.LocalClassDeclarationStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
@@ -86,7 +78,6 @@ public class ParseController {
 
     private static String buildName(MethodDeclaration md) {
         StringBuilder parameters = new StringBuilder();
-        StringBuilder parents = new StringBuilder();
         
         for(Parameter parameter:md.getParameters()) { // TODO include meta like final
             if(parameters.length() > 0) {
@@ -97,51 +88,13 @@ public class ParseController {
             parameters.append(parameter.getName());
         }
 
-        class NodeVisitor extends VoidVisitorAdapter<Object> {
-            public void visit(Node n, Object arg) {
-                System.out.println("The following node was unaccounted for: " + n.toString());
-            }
-
-            @Override
-            public void visit(MethodDeclaration md, Object arg) {
-                parents.insert(0, md.getName());
-                parents.insert(0, "::");
-            }
-
-            @Override
-            public void visit(ClassOrInterfaceDeclaration coid, Object arg) {
-                parents.insert(0, coid.getName());
-                parents.insert(0, "$");
-            }
-
-            @Override
-            public void visit(ObjectCreationExpr oce, Object arg) {
-                parents.insert(0, oce.getType().getName());
-                parents.insert(0, "$");
-            }
-
-            @Override
-            public void visit(CompilationUnit cu, Object arg) {
-                parents.deleteCharAt(0);
-            }
-
-            @Override
-            public void visit(BlockStmt bs, Object arg) {}
-            @Override
-            public void visit(LocalClassDeclarationStmt lcds, Object arg) {}
-            @Override
-            public void visit(MethodCallExpr mce, Object arg) {}
-            @Override
-            public void visit(ExpressionStmt es, Object arg) {}
+        String accessModifier = Modifier.getAccessSpecifier(md.getModifiers()).asString();
+        if (!accessModifier.equals("")) {
+            accessModifier = accessModifier + ' ';
         }
 
-        NodeVisitor v = new NodeVisitor();
-        Node ancestor = md;
-        while(ancestor.getParentNode().isPresent()) {
-            ancestor = ancestor.getParentNode().get();
-            ancestor.accept(v, null);
-        }
+        String returnType = md.getType().toString();
 
-        return parents.toString() + "::" + md.getName() + "(" + parameters.toString() + ")";
+        return accessModifier + returnType + ' ' + md.getName() + '(' + parameters.toString() + ");";
     }
 }
